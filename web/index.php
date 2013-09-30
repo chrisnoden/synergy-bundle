c62d4e404e7c2008f2b89371a75c1f04746c280e
Chris Noden <chris.noden@gmail.com>
2013-09-30 13:45:06 +0100

    Hack : tests the permissions of necessary files/folders in the web project

--- web/index.php
+++ web/index.php
@@ -46,16 +46,55 @@ setlocale(LC_ALL, 'en_GB');
  * This should be your main project directory (where your composer.json lives)
  */
 define('SYNERGY_ROOT_DIR', dirname(__DIR__));
-
+/**
+ * Not actually required by Synergy library but used for our Bundle - our app folder
+ */
+define('SYNERGY_APP_DIR', SYNERGY_ROOT_DIR . DIRECTORY_SEPARATOR . 'app');
+/**
+ * Not actually required by Synergy library but used for our Bundle - our log file
+ */
+define('SYNERGY_LOG_FILENAME', SYNERGY_APP_DIR . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . 'synergy.log');
+/**
+ * Not actually required by Synergy library but used for our Bundle - our temp/cache folder
+ */
+define('SYNERGY_TEMP_FOLDER', SYNERGY_APP_DIR . DIRECTORY_SEPARATOR . 'temp');
 
 /**
- * Load composer autoload.php
+ * Test our files/folders
  */
 if (!file_exists('../vendor/autoload.php')) {
     die(
-        'Must install vendor libraries using compose - see README.md'
+        "Must install vendor libraries using composer - see README.md\n"
     );
 }
+if (!file_exists(dirname(SYNERGY_LOG_FILENAME))) {
+    @mkdir(dirname(SYNERGY_LOG_FILENAME), 0777);
+    if (!file_exists(dirname(SYNERGY_LOG_FILENAME))) {
+        die(sprintf(
+            "Unable to create folder: %s\n", dirname(SYNERGY_LOG_FILENAME)
+        ));
+    }
+}
+if (!is_writable(dirname(SYNERGY_LOG_FILENAME))) {
+    die(sprintf(
+        "Unable to write to log file in: %s\n", SYNERGY_LOG_FILENAME
+    ));
+}
+if (!file_exists(SYNERGY_TEMP_FOLDER)) {
+    @mkdir(SYNERGY_TEMP_FOLDER, 0777);
+    if (!file_exists(SYNERGY_TEMP_FOLDER)) {
+        die(sprintf(
+            "Unable to create folder: %s\n", SYNERGY_TEMP_FOLDER
+        ));
+    }
+}
+if (!is_writable(SYNERGY_TEMP_FOLDER)) {
+    die(sprintf(
+        "Unable to write to temp folder in: %s\n", SYNERGY_TEMP_FOLDER
+    ));
+}
+
+
 require_once('../vendor/autoload.php');
 
 
@@ -86,7 +125,7 @@ $logger
             \Psr\Log\LogLevel::WARNING,
             \Psr\Log\LogLevel::DEBUG
         ),
-        \Talkback\Channel\ChannelFactory::File('/tmp/synergy.log'))
+        \Talkback\Channel\ChannelFactory::File(SYNERGY_LOG_FILENAME))
     ->addChannel(\Psr\Log\LogLevel::CRITICAL, \Talkback\Channel\ChannelFactory::Basic())
     ->addChannel(\Psr\Log\LogLevel::INFO, \Talkback\Channel\ChannelFactory::Growl('Bundle'));
 
@@ -100,6 +139,6 @@ $project =
         $logger
     );
 $project->setDev(true);
-$project->setAppDir(SYNERGY_ROOT_DIR . DIRECTORY_SEPARATOR . 'app');
+$project->setAppDir(SYNERGY_APP_DIR);
 $project->setConfigFilename($project->getAppDir() . DIRECTORY_SEPARATOR .'config'.DIRECTORY_SEPARATOR.'config.yml');
 $project->run();
